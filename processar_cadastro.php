73f2a58e9646eb44b2553ee0218f26fcb032c6c6
Alissonjoe <82912325+Alissonjoe@users.noreply.github.com>
2023-07-06 22:58:17 -0300

    Update processar_cadastro.php

--- processar_cadastro.php
+++ processar_cadastro.php
@@ -1,29 +1,48 @@
 <?php
+session_start(); // Inicia a sessão
 include "conexao.php";
 
-// Verifica se os dados do formulário foram enviados
+// Verifica se o formulário foi enviado
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    // Obtém os dados do formulário
-    $nome = $_POST["nome"];
+    // Obtém os valores enviados pelo formulário
     $email = $_POST["email"];
     $senha = $_POST["senha"];
 
-    // Insere os dados do novo usuário na tabela "usuarios"
-    $sql = "INSERT INTO usuarios (nome, email, senha) VALUES ('$nome', '$email', '$senha')";
+    // Conexão com o banco de dados
+    $conexao = new mysqli($servername, $username, $password, $dbname);
 
-    // Executa a consulta
-    if ($conexao->query($sql) === TRUE) {
-        // Cadastro bem-sucedido, redireciona para a página de lugares
-        header("Location: lugares.php");
-        exit();
-    } else {
-        // Se houver erro na inserção, exibe mensagem de erro
-        echo "Erro ao cadastrar o usuário: " . $conexao->error;
+    // Verifica se ocorreu algum erro na conexão
+    if ($conexao->connect_error) {
+        die("Falha na conexão com o banco de dados: " . $conexao->connect_error);
     }
+
+    // Prepara a consulta SQL para buscar o usuário com o e-mail fornecido
+    $sql = "SELECT * FROM usuarios WHERE email = '$email'";
+    $result = $conexao->query($sql);
+
+    // Verifica se o usuário foi encontrado
+    if ($result->num_rows === 1) {
+        $row = $result->fetch_assoc();
+        // Verifica se a senha fornecida corresponde à senha armazenada no banco de dados
+        if ($senha == $row["senha"]) {
+            // Autenticação bem-sucedida
+            // Define os dados do usuário na sessão
+            $_SESSION["usuario"] = $row["email"];
+            $_SESSION["nome"] = $row["nome"];
+            
+            // Redireciona para a página após o login bem-sucedido
+            header("Location: lugares.php");
+            exit;
+        } else {
+            // Senha inválida
+            echo "E-mail ou senha inválidos.";
+        }
+    } else {
+        // Usuário não encontrado
+        echo "E-mail ou senha inválidos.";
+    }
+
+    // Fecha a conexão com o banco de dados
+    $conexao->close();
 }
 ?>
-
-<!-- HTML do formulário -->
-<form action="processar_cadastro.php" method="POST">
-  <!-- Campos do formulário -->
-</form>
