9049e033ece938d9f9a40692843a5a80c5dccbbb
Alissonjoe <82912325+Alissonjoe@users.noreply.github.com>
2023-07-06 22:11:55 -0300

    Create rodape.php

---
+++ rodape.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Melhor Viagem</title>
+    <style>
+        body {
+            background-color: white;
+            margin: 0;
+            padding: 0;
+            font-family: Arial, sans-serif;
+        }
+
+        header {
+            background-color: white;
+            padding: 20px;
+            text-align: center;
+        }
+
+        footer {
+            background-color: while;
+            color: while;
+            padding: 20px;
+            text-align: center;
+        }
+    </style>
+</head>
+<body>
+    <header>
+        <h1>Melhor Viagem</h1>
+    </header>
+
+    <main>
+        <!-- Conteúdo do site aqui -->
+    </main>
+
+    <footer>
+        <p>Telefone: (xx) xxxx-xxxx | Endereço: Rua teste</p>
+    </footer>
+</body>
+</html>
