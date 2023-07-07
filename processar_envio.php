687d39685b8a6c1878f28d10d1cea36c809aac41
Alissonjoe <82912325+Alissonjoe@users.noreply.github.com>
2023-07-07 00:22:31 -0300

    Update processar_envio.php

--- processar_envio.php
+++ processar_envio.php
@@ -1,108 +1,48 @@
 <?php
 session_start(); // Inicia a sessão
-include "config.php";
+include "conexao.php";
 
 // Verifica se o formulário foi enviado
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-  // Verifica se todos os campos foram preenchidos
-  if (isset($_FILES["foto"]["tmp_name"]) && !empty($_FILES["foto"]["tmp_name"]) &&
-      isset($_POST["Lugar"]) && !empty($_POST["Lugar"]) &&
-      isset($_POST["dica"]) && !empty($_POST["dica"])) {
+    // Obtém os valores enviados pelo formulário
+    $email = $_POST["email"];
+    $senha = $_POST["senha"];
 
-    // Configurações da imagem redimensionada
-    $novaLargura = 600;
-    $novaAltura = 400;
-    $pastaUploads = 'uploads/'; // Pasta onde as imagens serão salvas
+    // Conexão com o banco de dados
+    $conexao = new mysqli($servername, $username, $password, $dbname);
 
-    // Move o arquivo enviado para o destino temporário
-    $caminhoTemporario = $_FILES["foto"]["tmp_name"];
-    $infoImagem = getimagesize($caminhoTemporario);
-
-    // Verifica se o arquivo enviado é uma imagem
-    if ($infoImagem !== false) {
-      // Obtém a extensão do arquivo
-      $extensao = pathinfo($_FILES["foto"]["name"], PATHINFO_EXTENSION);
-
-      // Cria um nome único para a imagem
-      $nomeImagem = uniqid('imagem_') . '.' . $extensao;
-
-      // Caminho completo para a imagem no diretório de uploads
-      $caminhoDestino = $pastaUploads . $nomeImagem;
-
-      // Carrega a imagem usando a biblioteca GD
-      if ($extensao == "jpg" || $extensao == "jpeg") {
-        $imagem = imagecreatefromjpeg($caminhoTemporario);
-      } elseif ($extensao == "png") {
-        $imagem = imagecreatefrompng($caminhoTemporario);
-      } elseif ($extensao == "gif") {
-        $imagem = imagecreatefromgif($caminhoTemporario);
-      } else {
-        // Se a extensão não for suportada, redirecione de volta para o formulário
-        header("Location: lugares.php");
-        exit;
-      }
-
-      // Obtém as dimensões atuais da imagem
-      list($larguraAtual, $alturaAtual) = $infoImagem;
-
-      // Cria uma nova imagem com as dimensões desejadas
-      $novaImagem = imagecreatetruecolor($novaLargura, $novaAltura);
-
-      // Redimensiona a imagem para as novas dimensões
-      imagecopyresampled($novaImagem, $imagem, 0, 0, 0, 0, $novaLargura, $novaAltura, $larguraAtual, $alturaAtual);
-
-      // Salva a nova imagem em um arquivo
-      imagejpeg($novaImagem, $caminhoDestino);
-
-      // Libera a memória ocupada pelas imagens
-      imagedestroy($imagem);
-      imagedestroy($novaImagem);
-
-      // Insira as informações no banco de dados
-      include "conexao.php";
-
-      $foto = $caminhoDestino;
-      $comentario = $_POST["Lugar"];
-      $dica = $_POST["dica"];
-
-      // Prepara a consulta SQL
-      $sql = "INSERT INTO tabela_destinos (foto, comentario, dica) VALUES (?, ?, ?)";
-      $stmt = $conexao->prepare($sql);
-     // Verifica se a preparação da consulta foi bem-sucedida
-      if ($stmt) {
-        // Vincula os parâmetros da consulta
-        $stmt->bind_param("sss", $foto, $comentario, $dica);
-
-        // Executa a consulta
-        if ($stmt->execute()) {
-          // Redireciona para a página de lugares após o envio bem-sucedido
-          header("Location: lugares.php");
-          exit;
-        } else {
-          // Se houver erro na execução da consulta, exibe mensagem de erro
-          echo "Erro ao enviar o destino: " . $stmt->error;
-        }
-
-        // Fecha a consulta
-        $stmt->close();
-      } else {
-        // Se houver erro na preparação da consulta, exibe mensagem de erro
-        echo "Erro ao preparar a consulta: " . $conexao->error;
-      }
-
-      // Fecha a conexão com o banco de dados
-      $conexao->close();
-
-    } else {
-      // Se o arquivo enviado não for uma imagem, redirecione de volta para o formulário
-      header("Location: lugares.php");
-      exit;
+    // Verifica se ocorreu algum erro na conexão
+    if ($conexao->connect_error) {
+        die("Falha na conexão com o banco de dados: " . $conexao->connect_error);
     }
-  } else {
-    // Se algum campo do formulário não foi preenchido, redirecione de volta para o formulário
-    header("Location: lugares.php");
-    exit;
-  }
+
+    // Prepara a consulta SQL para buscar o usuário com o e-mail fornecido
+    $sql = "SELECT * FROM usuarios WHERE email = '$email'";
+    $result = $conexao->query($sql);
+
+    // Verifica se o usuário foi encontrado
+    if ($result->num_rows === 1) {
+        $row = $result->fetch_assoc();
+        // Verifica se a senha fornecida corresponde à senha armazenada no banco de dados
+        if ($senha == $row["senha"]) {
+            // Autenticação bem-sucedida
+            // Define os dados do usuário na sessão
+            $_SESSION["usuario"] = $row["email"];
+            $_SESSION["nome"] = $row["nome"];
+            
+            // Redireciona para a página após o login bem-sucedido
+            header("Location: lugares.php");
+            exit;
+        } else {
+            // Senha inválida
+            echo "E-mail ou senha inválidos.";
+        }
+    } else {
+        // Usuário não encontrado
+        echo "E-mail ou senha inválidos.";
+    }
+
+    // Fecha a conexão com o banco de dados
+    $conexao->close();
 }
-  
 ?>
